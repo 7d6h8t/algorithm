4ef3185196c59fb189869b9213a79d307f785145
7d6h8t <7d6h8t@gmail.com>
2022-05-08 00:09:19 +0900

    + add EH8T::lower_bound

--- include/EH8T/algorithm.hpp
+++ include/EH8T/algorithm.hpp
@@ -14,10 +14,33 @@ namespace EH8T
         return last;
     }
 
+    template<typename _FwdIt, typename _Ty>
+    inline _FwdIt lower_bound(_FwdIt first, _FwdIt last, const _Ty& value)
+    {
+        _FwdIt it;
+        typename std::iterator_traits<_FwdIt>::difference_type count, step;
+        count = std::distance(first, last);
+
+        while (count > 0)
+        {
+            it = first;
+            step = count >> 1; // == count * 1/2^1;
+            std::advance(it, step);
+            if (*it < value)
+            {
+                first = ++it;
+                count -= step + 1;
+            }
+            else
+                count = step;
+        }
+        return first;
+    }
+
     template <typename _FwdIt, typename _Ty>
     inline bool binary_search(_FwdIt first, _FwdIt last, const _Ty& value)
     {
-        first = std::lower_bound(first, last, value);
+        first = EH8T::lower_bound(first, last, value);
 
         if (first == last)
             return false;
