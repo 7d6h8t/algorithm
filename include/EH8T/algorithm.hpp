61fa9c5f41d9ae64517d9a2e4464fb5038d4ceaf
7d6h8t <7d6h8t@gmail.com>
2022-05-11 02:08:03 +0900

    + add SFINAE
    : check if a template parameter is an iterator type or not

--- include/EH8T/algorithm.hpp
+++ include/EH8T/algorithm.hpp
@@ -1,9 +1,25 @@
 #pragma once
 #include <algorithm>
+#include <type_traits>
 
 namespace EH8T
 {
-    template <typename _InIt, typename _Ty>
+    template <typename _Ty, typename = void>
+    struct is_iterator : std::false_type
+    {};
+
+    template <typename _Ty>
+    struct is_iterator<_Ty, std::void_t<
+        typename std::iterator_traits<_Ty>::difference_type,
+        typename std::iterator_traits<_Ty>::pointer,
+        typename std::iterator_traits<_Ty>::reference,
+        typename std::iterator_traits<_Ty>::value_type,
+        typename std::iterator_traits<_Ty>::iterator_category>> : std::true_type
+    {};
+
+    template <typename _InIt,
+        typename _Ty,
+        typename = std::enable_if_t<is_iterator<_InIt>::value, int>>
     inline _InIt find(_InIt first, const _InIt last, const _Ty& value)
     {
         for (; first != last; ++first)
